3f7e2fd1b19375baba9de24d1a454c8c983430a8
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-01-31 02:59:27 +0300

    Implement superbowlWin() function to find the first win year

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;  // Exporting the function to make it available for testing
+  
